842d003005f3054776ca8197bf007743afca9680
ASIF TECHIE <167787463+AsifTEEch@users.noreply.github.com>
2024-11-19 07:35:34 +0530

    Add files via upload

---
+++ whiletask.java
@@ -0,0 +1,15 @@
+package ALLPROGRAM;
+
+public class whiletask {
+
+	    public static void main(String[] args) {
+	        int i = 10; // intialization of i
+
+	        while (i <= 100) { 
+	            System.out.println(i); 
+	            i += 10; // increment of 10
+	        }
+	    }
+	
+
+}
